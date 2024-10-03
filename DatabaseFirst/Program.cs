c6fae838d09580ccac8917af881c039aa1f26a8d
nidakul <nidakul@hotmail.com>
2024-10-03 17:16:53 +0300

    end of lesson9

--- DatabaseFirst/Program.cs
+++ DatabaseFirst/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//dotnet ef dbcontext scaffold "Server=localhost;Database=Northwind;User Id=SA;Password=rentacardb;TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer
---
+++ Lesson9/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+Console.WriteLine("Hello, World!");
+
+
+public class ECommerce : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //Provider
+        //ConnectionString
+        //LazyLoading
+        optionsBuilder.UseSqlServer("Server = localhost; Database = ECommerceDb; User Id = SA; Password = rentacardb; TrustServerCertificate=true");
+    }
+}
+
+public class Product
+{
+    //ef core default primary keys
+    //public int Id { get; set; } 
+    //public int ID { get; set; }
+    //public int ProductId { get; set; }
+    public int ProductID { get; set; }
+}
