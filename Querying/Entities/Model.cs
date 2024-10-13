731bd2a260911eb3c719cd50a20908aac219d658
nidakul <nidakul@hotmail.com>
2024-10-13 17:04:12 +0300

    end of lessson13

---
+++ Querying/Entities/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Querying.Entities;
+
+public partial class Model
+{
+    public int Id { get; set; }
+
+    public int BrandId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal DailyPrice { get; set; }
+
+    public string ImageUrl { get; set; } = null!;
+
+    public virtual Brand Brand { get; set; } = null!;
+}
--- Querying/Program.cs
+++ Querying/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Querying.Contexts;
+using Querying.Entities;
 
+NorthwindContext context = new NorthwindContext();
+
+#region En temel sorgulama
+#region Method Syntax
+//List<Product> products = await context.Products.ToListAsync();
+//foreach(Product product in products)
+//{
+//Console.WriteLine(product.ProductName); 
+//}
+#endregion
+#region Query Syntax
+//List<Product> products = await (from product in context.Products
+//                          select product).ToListAsync();
+//foreach(Product p in products)
+//Console.WriteLine(p.ProductName);
+#endregion
+#endregion
+
+#region Deferred Execution(Ertelenmiş Çalışma)
+//Burada verileri productId = 200'e göre getirir.
+int productId = 5;
+string productName = "rd";
+var products = from product in context.Products
+               where product.ProductId > productId && product.ProductName.Contains(productName)
+               select product;
+productId = 200;
+productName = "st";
+foreach (Product p in products)
+{
+    Console.WriteLine(p.ProductName + " => " + p.ProductId);
+}
+#endregion
