06f862bc2070f4ba551eceecf34bf0f78e714c49
nidakul <nidakul@hotmail.com>
2024-10-18 22:55:34 +0300

    started lesson19

--- ChangeTracker/Program.cs
+++ ChangeTracker/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Change Tracking Nedir?
+//Context nesnesi üzerinden gelen tüm nesneler/veriler otomatik olarak bir takip mekanizması tarafından izlenirler.
+//İşte bu takip mekanizmasına Change Tracker denir. Change tracker ile nesneler üzerindeki değişiklikler/işlemler takip edilerek
+//netice itibariyle bu işlemlerin fıtratına uygun sql sorgucukları generate edilir. İşte bu işleme de Change Tracking denir. 
+#endregion
 
