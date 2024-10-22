64ad1d88742c96578ea94275df54b12895f94fc0
nidakul <nidakul@hotmail.com>
2024-10-22 22:11:13 +0300

    DetectChanges

--- ChangeTracker/Program.cs
+++ ChangeTracker/Program.cs
@@ -12,12 +12,29 @@ NorthwindContext context = new NorthwindContext();
 //Takip edilen nesnelere erişebilmemizi sağlayan ve gerektirdiği taktirde gerçekleştirmemizi sağlayan bir propertydir.
 //Context sınıfının base class'ı olan DbContext sınıfının bir member'ıdır.
 
-var products = await context.Products.ToListAsync();
-products[6].UnitPrice = 123; //update
-context.Products.Remove(products[7]); //delete
-products[8].ProductName = "Kalem"; //update
+//var products = await context.Products.ToListAsync();
+//products[6].UnitPrice = 123; //update
+//context.Products.Remove(products[7]); //delete
+//products[8].ProductName = "Kalem"; //update
 
-var datas = context.ChangeTracker.Entries(); //Takip edilen tüm nesneleri getirir
+//var datas = context.ChangeTracker.Entries(); //Takip edilen tüm nesneleri getirir
 
+//await context.SaveChangesAsync();
+#endregion
+
+#region DetectChanges Metodu
+//Ef Core, context nesnesi tarafından izlenen tüm nesnelerdeki değişiklikleri Change Tracker sayesinde takip edebilmekte ve nesnelerde olan
+//verisel değişiklikler yakalanarak bunların anlık görüntüleri(snapshot)'ini oluşturabilir.
+//Yapılan değişikliklerin veritabanına gönderilmeden önce algılandığından emin olmak gerekir.SaveChanges fonksiyonı çağrıldığı anda
+//nesneler Ef Core tarafından otomatik kontrol edilirler.
+//Ancak yapılan operasyonlarda güncel tracking verilerinden emin olabilmek için değişikliklerin algılanmasını opsiyonel olarak gerçekleştirmek
+//isteyebiliriz. İşte bunun için DetectChanges fonksiyonu kullanılabilir ve her ne kadar Ef Core değişiklikleri otomatik algılıyor olsa da siz
+//yine de iradenizle kontrole zorlayabilirsiniz.
+
+var products = await context.Products.FirstOrDefaultAsync(p => p.ProductId == 3);
+products.UnitPrice = 345;
+
+context.ChangeTracker.DetectChanges();
 await context.SaveChangesAsync();
+
 #endregion
