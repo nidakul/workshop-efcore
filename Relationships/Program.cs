79763ce56263a7acb9b7e8e31653adb49130c757
nidakul <nidakul@hotmail.com>
2024-10-25 01:13:57 +0300

    end of Relationship terms

---
+++ Relationships/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine();
+
+#region Relationship(İlişkiler) Terimleri
+#region Principal Entity(Asıl Entity)
+//Kendi başına var olabilen tabloyu modelleyen entity'e denir.
+#endregion
+#region Dependent Entity(Bağımlı Entity)
+//Kendi başına var olamayan, bir başka tabloya bağımlı(ilişkisel olarak bağımlı) olan tabloyu modelleyen entity'e denir.
+#endregion
+#region Foreign Key
+//Principal Entity ile Dependent Entity arasındaki ilişkiyi sağlayan key'dir.
+//Dependent Entity'de tanımlanır.
+//Principal Entity'de ki Principal Key'i tutar.
+#endregion
+#region Principal Key
+//Principal Entity'deki id'nin kendisidir. Principal Entity'nin kimliği olan kolonu ifade eden property'dir.
+#endregion
+#region Navigation Property
+//İlişikisel tablolar arasındaki fiziksel erişimi entity class'ları üzerinden sağlayan property'lerdir.
+//public Departman Departman {get;set;}
+//public ICollection<Calisan> Calisanlar { get; set; }
+//Bir property'nin navigation property olabilmesi için kesinlikler entity türünden olması gerekir.
+//Navigation property'ler entitylerdeki tanımlarına göre n'e n yahut 1'e n şeklinde ilişki türlerini ifade etmektedirler.
+#endregion
+#endregion
